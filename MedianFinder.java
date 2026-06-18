acbd40c298a1ff7a1b75bd7f92c56e4485d52e2a
ALADI ARUN M <143047232+aladiarunm@users.noreply.github.com>
2026-06-18 12:57:03 +0530

    Implement MedianFinder class with Heap
    Implemented a class to maintain a dynamic list of numbers and calculate the median efficiently using two heaps.

---
+++ MedianFinder.java
@@ -0,0 +1,33 @@
+class MedianFinder {
+
+    PriorityQueue<Integer> minHeap;
+    PriorityQueue<Integer> maxHeap;
+
+    public MedianFinder() {
+        minHeap = new PriorityQueue<>();
+        maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+    }
+    
+    public void addNum(int num) {
+        maxHeap.add(num);
+        minHeap.add(maxHeap.poll());
+        if(maxHeap.size()+1<minHeap.size()){
+            maxHeap.add(minHeap.poll());
+        }
+    }
+    
+    public double findMedian() {
+        if(minHeap.size() == maxHeap.size()){
+            return (minHeap.peek() + maxHeap.peek())/2.0;
+        }else{
+            return minHeap.peek()/1.0;
+        }
+    }
+}
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder obj = new MedianFinder();
+ * obj.addNum(num);
+ * double param_2 = obj.findMedian();
+ */
